d7a0ce372e36ca1e4d6c5ebdc0c6858a1f5174bf
Eliezer Castro <eliezergino@gmail.com>
2023-11-14 00:02:35 -0300

    removendo o comentario

--- src/models/Historico.ts
+++ src/models/Historico.ts
@@ -4,7 +4,6 @@ const historicoSchema = new mongoose.Schema({
   pedidoId: mongoose.Schema.Types.ObjectId,
   dataHora: Date,
   status: String,
-  // outros campos conforme necessário
 });
 
 const Historico = mongoose.model('Historico', historicoSchema);
