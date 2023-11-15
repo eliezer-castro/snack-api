f8ea35b144e97fc11013e10131fc42f4b30c5144
Eliezer Castro <eliezergino@gmail.com>
2023-11-15 10:35:25 -0300

    removendo importação não utilizada

--- src/routes/pedidoRoutes.ts
+++ src/routes/pedidoRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { criarPedido, listarPedidos, obterPedidos, atualizarPedido, deletarPedido } from "../controllers/PedidoController";
+import { criarPedido, obterPedidos, atualizarPedido, deletarPedido } from "../controllers/PedidoController";
 const pedidosRoutes = Router();
 
 pedidosRoutes.post('/sessao/:sessaoId', criarPedido);
