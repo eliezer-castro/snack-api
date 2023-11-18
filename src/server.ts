8eff830bf0709457357789397621d1b6ea2bc648
Eliezer Castro <eliezergino@gmail.com>
2023-11-18 10:41:08 -0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,39 +1,39 @@
-require('dotenv').config();
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import { pedidosRoutes } from './routes/pedidoRoutes';
-import { sessaoRoutes } from './routes/sessaoRoutes';
-import { produtosRoutes } from './routes/produtosRoutes';
+require("dotenv").config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { pedidosRoutes } from "./routes/pedidoRoutes";
+import { sessaoRoutes } from "./routes/sessaoRoutes";
+import { produtosRoutes } from "./routes/produtosRoutes";
 
 const app = express();
-app.use(cors(
-  {
-    origin: 'http://127.0.0.1:5173',
-    optionsSuccessStatus: 200
-  }
-));
+app.use(cors({
+  origin: "*",
+  methods: "*",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 app.use(express.json());
 
-
-app.use('/pedidos', pedidosRoutes);
-app.use('/sessao', sessaoRoutes);
-app.use('/produtos', produtosRoutes);
-
+app.use("/pedidos", pedidosRoutes);
+app.use("/sessao", sessaoRoutes);
+app.use("/produtos", produtosRoutes);
 
 const CONNECTION_STRING = process.env.MONGODB_URI;
 
 if (!CONNECTION_STRING) {
-  console.error('Erro: A string de conexão com o MongoDB não está definida.');
+  console.error("Erro: A string de conexão com o MongoDB não está definida.");
   process.exit(1);
 }
 
-mongoose.connect(CONNECTION_STRING).then(() => {
-  console.log('Conectado ao MongoDB');
-}).catch(err => {
-  console.error('Erro ao conectar com o MongoDB', err);
-});
-
+mongoose
+  .connect(CONNECTION_STRING)
+  .then(() => {
+    console.log("Conectado ao MongoDB");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar com o MongoDB", err);
+  });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
