4b9ae8469b99690b603e703e8b461a88a120d103
Eliezer Castro <eliezergino@gmail.com>
2023-11-18 10:36:45 -0300

    add optionsSuccessStatus

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,8 @@ import { produtosRoutes } from './routes/produtosRoutes';
 const app = express();
 app.use(cors(
   {
-    origin: 'http://127.0.0.1:5173'
+    origin: 'http://127.0.0.1:5173',
+    optionsSuccessStatus: 200
   }
 ));
 app.use(express.json());
